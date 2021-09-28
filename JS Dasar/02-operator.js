a722419b1328994e43a906be5bc56b0680e13b0e
Singgih Pratama <61181279+Shinjixy@users.noreply.github.com>
2021-09-28 14:15:17 +0700

    Add files via upload

---
+++ JS Dasar/02-operator.js
@@ -0,0 +1,49 @@
+/// Soal - 01
+/// Dibawah ini akan ditampilkan daftar pengeluaran bulan ini beserta gaji yang akan diterima, tugas-mu adalah untuk menghitung gaji yang tersisa
+
+/// Pegeluaran
+/// gundam = 750000
+/// makan = 2000000
+/// kost = 1000000
+/// subscription = 250000
+
+/// Gaji
+/// gajiPokok = 5000000
+/// gajiBonus = 10% dari gajiPokok
+/// gajiLembur = 5% dari gajiPokok
+
+/// Soal - 01 Sisa Gaji
+let gundam = 750000, makan = 2000000, kost = 1000000, subcription = 250000; // Declare Variable and Value of Spent
+let gajiPokok = 5000000, gajiBonus, gajiLembur // Declare Variable or Value of Income
+
+let pengeluaran, pendapatan; // Declare Variable for subtitute
+
+gajiBonus = gajiPokok * (10 / 100); // Calculation of gajiBonus
+gajiLembur = gajiPokok * (5 / 100); // Calculation of gajiLembur
+
+pendapatan = gajiPokok + gajiLembur + gajiBonus; // Calculate Revenue
+pengeluaran = gundam + makan + kost + subcription; // Calculate Expense
+
+const sisaGaji = pendapatan - pengeluaran; // Calculate Resudual Revenue
+hasil = new Intl.NumberFormat("ID").format(sisaGaji); // Convert into Currency Numbering
+
+console.log(hasil); // Display result "1.750.000"
+
+/// Soal - 02
+/// Gunakan operator yang tepat untuk mengetahui apakah 2 variabel dibawah ini memiliki nilai yang sama (tipe data tidak termasuk)
+const oneInString = "1"; // Declare Variable with value
+const oneInNumber = 1; // Declare Variable with value
+const isSame = oneInString == oneInNumber; // Comparing value with their data type
+console.log(isSame); // Display result "true"
+
+/// Soal - 03
+/// Gunakan operator yang tepat untuk mengetahui apakah studentA memiliki nilai yang lebih tinggi atau sama dengan studentB
+const studentA = 75; // Declare Variable with value
+const studentB = 80; // Declare Variable with value
+const isHigher = studentA >= studentB; // Comparing value with their data type
+console.log(isHigher); // Display result "false"
+
+/// Soal - 04
+/// Gunakan operator yang tepat untuk membedakan 2 buah variable yang terdapat pada soal-02 berdasarkan value-nya (tipe data juga dihitung)
+const final = oneInNumber === oneInString; // Comparingn value with their data type
+console.log(final);
---
+++ JS Dasar/03-condition.js
@@ -0,0 +1,61 @@
+/// Soal - 01
+/// Buatlah sebuah kondisi dimana akan melakukan pengecekan terhadap user yang hendak bepergian menggunakan motor, ada beberapa hal yang perlu dilakukan salah satu-nya adalah
+/// - kondisi dibuat menggunakn IF ELSE
+/// - kondisi pertama akan mengecek apakah user sudah berusia kurang dari 18 tahun
+/// - jika IYA, maka cetak tulisan "Anda belum cukup umur"
+/// - jika TIDAK
+///   buatlah kondisi lagi dimana akan melakukan cek terhadap user tersebut, apakah dia sudah memiliki SIM atau belum
+///   - jika IYA, maka cetak tulisan "Selamat berkendara"
+///   - jika Tidak, maka cetak tulisan "Diharapkan untuk membuat SIM terlebih dahulu"
+
+const userAge = 18;
+const drivingLicense = true;
+/// EDIT HERE
+
+// kondisi jika umur < 18 tahun
+if(userAge < 18) {
+    console.log("Anda Belum Cukup Umur")
+} // kondisi jika umur > 18 tahun
+else{
+    if(drivingLicense == true) { // kondisi jika sudah memiliki SIM
+        console.log("Selamat Berkendara")
+    }
+    else{ // kondisi jika belum memiliki SIM
+        console.log("Diharapkan untuk Membuat SIM terlebih Dahulu")
+    }
+}
+
+
+
+
+/// Soal - 02
+/// Buatlah sebuah kondisi yang akan melakukan konversi suhu dari celcius ke variable yang dituju (kelvin, fahrenheit, dan celsius)
+/// - kondisi dibuat menggunakan switch case
+/// - konversi celcius menjadi kelvin apabila user memasukan unit berupa "K"
+/// - konversi celcius menjadi fahrenheit apabila user memasukan unit berupa "F"
+/// - konversi celcius menjadi celcius apabila user memasukan unit berupa "C"
+/// - menampilkan pesan "Maaf anda salah input", apabila variable tidak memenuhi semua kondisi yang ada
+const temperature = 100; /// suhu
+const unit = "F"; /// inputan
+switch (unit) {
+  /// EDIT HERE
+
+  // perhitungan celcius ke Kelvin
+  case "K":
+      console.log(temperature + 273)
+      break;
+
+    // perhitungan celcius ke Fahrenheit
+    case "F":
+        console.log((9/5*temperature) + 32)
+        break;
+    
+    // perhitungan celcius ke Celcius
+    case "C":
+        console.log(C)
+        break;
+
+    // Jika salah input
+    default:
+        console.log("Maaf Anda Salah Input")
+}
